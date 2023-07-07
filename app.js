1975c89036ba8cc63fc5982e68cdbcc0a7e4a0fc
ciao0603 <jocelyn94032.0@gmail.com>
2023-07-07 11:05:55 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({_id: 'desc'})
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
