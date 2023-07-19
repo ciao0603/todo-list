930910a18a9c54fdc5f509e12d47dc6641434cac
ciao0603 <jocelyn94032.0@gmail.com>
2023-07-19 18:58:25 +0800

    feat: add relation between User and Todo models

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -8,44 +8,50 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+  const userId = req.user._id
   const name = req.body.name
-  Todo.create({ name })
+
+  Todo.create({ name, userId })
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  Todo.findById(id)
-    .lean()
+  const userId = req.user._id
+  const _id = req.params.id
+  Todo.findOne({ _id, userId }) //先找出id一樣的todo > 確保屬於目前登入的user
+     .lean()
     .then(todo => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  Todo.findOne({ _id, userId })
     .lean()
     .then(todo => res.render('edit', { todo }))
     .catch(error => console.log(error))
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const userId = req.user._id
+  const _id = req.params.id
   const { name, done } = req.body
-  Todo.findById(id)
+  Todo.findOne({ _id, userId })
     .then(todo => {
       todo.name = name
       todo.done = done === 'on' //true
       return todo.save()
     })
-    .then(todo => res.redirect(`/todos/${id}`))
+    .then(todo => res.redirect(`/todos/${_id}`))
     .catch(error => console.log(error))
 })
 
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  Todo.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  Todo.findOne({ _id, userId })
     .then(todo => todo.remove())
     .then(todo => res.redirect('/'))
     .catch(error => console.log(error))
