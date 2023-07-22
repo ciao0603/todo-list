f051f6ad21ac7ee7f7a89dd99f7466c0291ce653
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-22 08:59:52 +0800

    feat: modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,9 +1,32 @@
+const bcrypt = require('bcryptjs')
+
 const db = require('../../config/mongoose')
 const Todo = require('../todo') // 載入 todo model
+const User = require('../user')
+
+const SEED_USER = {
+  name: 'EX',
+  email: 'ex@example.com',
+  password: '123456789'
+}
 
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` })
-  }
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash,
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({ name: `name-${i}`, userId })))
+    })
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
 })
